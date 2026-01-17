ea681a4c6bfdb715c3d7eb83fe11b02a7adc74b1
ivangarzab <ivangb6@gmail.com>
2026-01-17 11:39:18 -0800

    ci: Move the TestFlight post clone script to the "right" place

--- iosApp/ci_scripts/ci_post_clone.sh
+++ iosApp/ci_scripts/ci_post_clone.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Fail the build if any command fails
-set -e
+# Move up to the repository root to find gradlew
+cd ..
+chmod +x gradlew
 
-# Install and set Java 17
+# Install Java 17
 brew install openjdk@17
 sudo ln -sfn /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk-17.jdk/Contents/Home"
 
-echo "JAVA_HOME is set to $JAVA_HOME"
-java -version
+echo "✅ Environment ready. JAVA_HOME is $JAVA_HOME"
