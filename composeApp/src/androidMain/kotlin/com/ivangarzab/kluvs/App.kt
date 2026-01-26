686987ef4f318dcba20731ce2300ea02e2b1c5a0
ivangarzab <ivangb6@gmail.com>
2026-01-25 22:10:00 -0800

    refactor(android): Refactor App to train barK logger differently for DEBUG and RELEASE - Implement BuildConfig into :composeApp module

--- composeApp/src/androidMain/kotlin/com/ivangarzab/kluvs/App.kt
+++ composeApp/src/androidMain/kotlin/com/ivangarzab/kluvs/App.kt
@@ -2,14 +2,30 @@ package com.ivangarzab.kluvs
 
 import android.app.Application
 import com.ivangarzab.bark.Bark
+import com.ivangarzab.bark.Level
 import com.ivangarzab.bark.trainers.AndroidLogTrainer
 import com.ivangarzab.kluvs.di.initKoin
 import org.koin.android.ext.koin.androidContext
 
 class App : Application() {
+
     override fun onCreate() {
         super.onCreate()
-        Bark.train(AndroidLogTrainer())
+        initLogging()
+        initDependencyInjection()
+    }
+
+    private fun initLogging() = Bark.train(
+        AndroidLogTrainer(
+            if (BuildConfig.DEBUG) {
+                Level.VERBOSE
+            } else {
+                Level.WARNING
+            }
+        )
+    )
+
+    private fun initDependencyInjection() {
         initKoin {
             androidContext(this@App)
         }
