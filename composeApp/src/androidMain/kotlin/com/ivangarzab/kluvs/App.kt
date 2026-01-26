4abb3f0128cbefb18fabb8f04a67b0e18b7e1979
ivangarzab <ivangb6@gmail.com>
2026-01-26 09:25:58 -0800

    feat: Create new SentryTrainer for barK and implement into Android's App

--- composeApp/src/androidMain/kotlin/com/ivangarzab/kluvs/App.kt
+++ composeApp/src/androidMain/kotlin/com/ivangarzab/kluvs/App.kt
@@ -6,6 +6,7 @@ import com.ivangarzab.bark.Level
 import com.ivangarzab.bark.trainers.AndroidLogTrainer
 import com.ivangarzab.kluvs.app.initializeSentry
 import com.ivangarzab.kluvs.di.initKoin
+import com.ivangarzab.kluvs.util.SentryTrainer
 import org.koin.android.ext.koin.androidContext
 
 class App : Application() {
@@ -17,15 +18,14 @@ class App : Application() {
         initDependencyInjection()
     }
 
-    private fun initLogging() = Bark.train(
-        AndroidLogTrainer(
-            if (BuildConfig.DEBUG) {
-                Level.VERBOSE
-            } else {
-                Level.WARNING
-            }
+    private fun initLogging() = Bark.apply {
+        train(
+            AndroidLogTrainer(
+                if (BuildConfig.DEBUG) Level.VERBOSE else Level.WARNING
+            )
         )
-    )
+        train(SentryTrainer())
+    }
 
     private fun initDependencyInjection() {
         initKoin {
