fc73922b6b3db9712bb5c09ddda6382b0f6fa5ab
ivangarzab <ivangb6@gmail.com>
2026-01-27 19:16:39 -0800

    feat: Set up barK in iOS including the SentryTrainer()

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -6,6 +6,7 @@ struct iOSApp: App {
 
     init() {
         SentrySetupKt.initializeSentry()
+        startLogging()
         KoinHelperKt.doInitKoin()
     }
 
@@ -17,5 +18,17 @@ struct iOSApp: App {
                     OAuthCallbackHandler.shared.handleCallback(url)
                 }
         }
+    }
+    
+    private func startLogging() {
+        #if DEBUG
+        let volume = Level.verbose
+        #else
+        let volume = Level.debug
+        #endif
+        
+        BarkConfig().autoTagDisabled = false
+        Bark().train(trainer: NSLogTrainer(volume: volume))
+        Bark().train(trainer: SentryTrainer())
     }
 }
