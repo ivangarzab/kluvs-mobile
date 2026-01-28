4a140da31682edfb159221ca112cb9fd4d688bad
ivangarzab <ivangb6@gmail.com>
2026-01-27 22:54:53 -0800

    refactor: Implement coroutine scope into the SentryTrainer for asynchronous calls

--- shared/src/commonMain/kotlin/com/ivangarzab/kluvs/util/SentryTrainer.kt
+++ shared/src/commonMain/kotlin/com/ivangarzab/kluvs/util/SentryTrainer.kt
@@ -6,6 +6,11 @@ import com.ivangarzab.bark.Trainer
 import io.sentry.kotlin.multiplatform.Sentry
 import io.sentry.kotlin.multiplatform.SentryLevel
 import io.sentry.kotlin.multiplatform.protocol.Breadcrumb
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 /**
  * A barK Trainer that pipes logs into Sentry.
@@ -20,35 +25,38 @@ class SentryTrainer : Trainer {
 
     override val pack = Pack.CUSTOM
 
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
     override fun handle(
         level: Level,
         tag: String,
         message: String,
         throwable: Throwable?
     ) {
-        // Add as a Breadcrumb for historical context (up to 100 attached on crash report)
-        val crumb = Breadcrumb().apply {
-            this.category = tag
-            this.message = message
-            this.level = when (level) {
-                Level.WARNING -> SentryLevel.WARNING
-                Level.ERROR -> SentryLevel.ERROR
-                Level.CRITICAL -> SentryLevel.FATAL
-                else -> SentryLevel.INFO // Covers INFO, and technically DEBUG/VERBOSE if they ever leak
+        scope.launch {
+            // Add as a Breadcrumb for historical context (up to 100 attached on crash report)
+            val crumb = Breadcrumb().apply {
+                this.category = tag
+                this.message = message
+                this.level = when (level) {
+                    Level.WARNING -> SentryLevel.WARNING
+                    Level.ERROR -> SentryLevel.ERROR
+                    Level.CRITICAL -> SentryLevel.FATAL
+                    else -> SentryLevel.INFO // Covers INFO, and technically DEBUG/VERBOSE if they ever leak
+                }
             }
-        }
-        Sentry.addBreadcrumb(crumb)
+            Sentry.addBreadcrumb(crumb)
 
-        // Report actual exceptions/errors
-        // We only "capture" (send to Sentry's servers immediately) ERROR and CRITICAL.
-        if (level == Level.ERROR || level == Level.CRITICAL) {
-            if (throwable != null) {
-                Sentry.captureException(throwable)
-            } else {
-                // If there is no exception object, capture the message as an Error event
-                Sentry.captureMessage("[$tag] $message")
+            // Report actual exceptions/errors
+            // We only "capture" (send to Sentry's servers immediately) ERROR and CRITICAL.
+            if (level == Level.ERROR || level == Level.CRITICAL) {
+                if (throwable != null) {
+                    Sentry.captureException(throwable)
+                } else {
+                    // If there is no exception object, capture the message as an Error event
+                    Sentry.captureMessage("[$tag] $message")
+                }
             }
         }
     }
-
 }
