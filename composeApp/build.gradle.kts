527e98e251883c68a702da68f6c6931fa6c46f36
ivangarzab <ivangb6@gmail.com>
2026-01-30 23:23:41 -0800

    refactor(android): Implement Android splashscreen for light & dark themes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -20,6 +20,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.splashscreen)
         }
         androidInstrumentedTest.dependencies {
             implementation(libs.androidx.test.junit)
